4e1a156b4f8d02238ebaa68ecf030456d7c4b561
bineea <33547731+bineea@users.noreply.github.com>
2020-04-14 23:09:49 +0800

    配置hystrix

--- src/main/java/my/blog/account/AccountApplication.java
+++ src/main/java/my/blog/account/AccountApplication.java
@@ -2,6 +2,7 @@ package my.blog.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 //@SpringBootApplication 告诉Spring Boot框架，这是项目的引导类。实现服务的核心初始化逻辑
@@ -9,6 +10,9 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 //允许访问/refresh端点，强制Spring Boot应用重新读取程序配置
 @RefreshScope
+
+//启用Hystrix
+@EnableCircuitBreaker
 public class AccountApplication {
 
     public static void main(String[] args) {
--- src/main/java/my/blog/account/services/UserServiceImpl.java
+++ src/main/java/my/blog/account/services/UserServiceImpl.java
@@ -1,9 +1,13 @@
 package my.blog.account.services;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import my.blog.account.resources.entity.User;
 import my.blog.account.resources.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Random;
 
 @Service
 public class UserServiceImpl extends  AbstractService implements  UserService{
@@ -11,8 +15,10 @@ public class UserServiceImpl extends  AbstractService implements  UserService{
     @Autowired
     private UserRepo userRepo;
 
+    @HystrixCommand
     @Override
     public User findById(String userId) {
         return userRepo.findById(userId).orElse(null);
     }
+
 }
